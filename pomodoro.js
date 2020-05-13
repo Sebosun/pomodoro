d05b55a96e294e376394882235db7666251a4724
Sebosun <sebos@protonmail.com>
2020-05-13 16:16:18 +0200

    added a simple countdown from seconds, no ui yet

--- pomodoro.js
+++ pomodoro.js
@@ -1,1 +1,27 @@
-console.log('hello')
+let startInt = setInterval(simpleTimer, 1000);
+let seconds = 0;
+let minutes = 0;
+let startingTime = 2;
+
+let timerSelector = document.querySelector('#timer')
+
+// 100 seconds - > 01 min 40 sec
+// 200 seconds - > 03 min 20 sec etc...
+function convertToMinuteSeconds(seconds){
+    
+}
+
+function simpleTimer(){
+    startingTime -= 1;
+    timerSelector.textContent = startingTime;
+    //if time reaches 0, calls stop function will will, well, stop timer
+    if (startingTime == 0){
+        stopFunction();
+    }
+}
+
+function stopFunction(){
+    clearInterval(startInt)
+}
+
+startInt;
