c4ed5c56bc99de3ee479636c762ba7eb1d839d02
Sebosun <sebos@protonmail.com>
2020-05-14 00:18:04 +0200

    fixed minor bug where the 00:00 number wouldnt show. `Fooling` the user may be necessary to achieve the 00:00  by adding 1 more second to their timer

--- pomodoro.js
+++ pomodoro.js
@@ -1,10 +1,16 @@
 let seconds = 0;
 let minutes = 0;
 
-let startingTime = 150;
+let startingTime = 61;
+// possible necessary step, add 1 more second to the timer, so that 00:00 can be shown. fools the user a bit tho.
+
+const workTime = 2;
+const breakTime = 2;
+
 let timeString = '';
 let timerEvent;
-
+let rounds = 5;
+let currentMode = 'work';
 
 // let startInter = setInterval(simpleCountdown, 1000);
 
@@ -12,38 +18,66 @@ const timerSelector = document.querySelector('#timer');
 const buttonSelector = document.querySelectorAll('.buttons');
 
 //toberemoved
-timerSelector.textContent = convertToMinuteSeconds(startingTime);
+timerSelector.textContent = convertToMinuteSeconds(startingTime - 1);
 
 
 //takes in raw seconds passed, returns time as a string in MM:SS format
 function convertToMinuteSeconds(seconds){  
-    let minutes = Math.floor(seconds / 60);
-    let remainingSeconds = seconds - minutes * 60;
-
-    if (minutes.toString().length === 1){
-        let minutesArray = minutes.toString().split('');
-        minutesArray.unshift('0')
-        minutes = minutesArray.join('')
+    if (seconds === 0){
+        return '00:00'
     }
-    if (remainingSeconds.toString().length === 1){
-        let secondsArray = remainingSeconds.toString().split('');
-        secondsArray.unshift('0')
-        remainingSeconds = secondsArray.join('')
-        console.log(remainingSeconds)
-    }
+    else{
+        let minutes = Math.floor(seconds / 60);
+        let remainingSeconds = seconds - minutes * 60;
 
-    timeString = `${minutes}:${remainingSeconds}`
-    
-    return timeString;
+        if (minutes.toString().length === 1){
+            let minutesArray = minutes.toString().split('');
+            minutesArray.unshift('0')
+            minutes = minutesArray.join('')
+        }
+        if (remainingSeconds.toString().length === 1){
+            let secondsArray = remainingSeconds.toString().split('');
+            secondsArray.unshift('0')
+            remainingSeconds = secondsArray.join('')
+            console.log(remainingSeconds)
+        }
+
+        timeString = `${minutes}:${remainingSeconds}`
+        
+        return timeString;
+    }
 }
 
 function simpleCountdown(){
     startingTime -= 1;
-    timerSelector.textContent = convertToMinuteSeconds(startingTime);
-    //    let convertedTime = convertToMinuteSeconds(startingTime);
-    //if time reaches 0, calls stop function will, well, stop timer
+    timerSelector.textContent = convertToMinuteSeconds(startingTime - 1)
+
     if (startingTime == 0){
-        stopFunction();
+        timerSelector.textContent = convertToMinuteSeconds(startingTime - 1);
+        if (rounds >= 1){
+            if (currentMode === 'work'){
+                timerSelector.textContent = convertToMinuteSeconds(startingTime - 1)
+                currentMode = 'break';
+                timerSelector.style.color = 'green';
+                startingTime = breakTime;
+                rounds -= 1;
+                timerSelector.textContent = convertToMinuteSeconds(startingTime -1);
+            }
+            else{
+                currentMode = 'work';
+                timerSelector.style.color = 'red';
+                startingTime = workTime;
+                rounds -= 1;
+                timerSelector.textContent = convertToMinuteSeconds(startingTime-1);
+            }
+        }
+        else if (rounds === 0){
+            stopFunction(timerEvent);
+            alert('Times over');
+            rounds = 5;
+            startingTime = workTime;
+            timerSelector.textContent = convertToMinuteSeconds(startingTime);
+        }
     }
 }
 //making this a function since declaring startInter just starts the timer immediatelly
