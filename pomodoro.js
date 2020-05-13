d7597a86f8ee3236fa846b3b89b350e6ab84517b
Sebosun <sebos@protonmail.com>
2020-05-13 16:45:00 +0200

    added convertToMinutesSeconds, which takes seconds and returns a string of minutes and seconds in a nice MM:SS format

--- pomodoro.js
+++ pomodoro.js
@@ -1,20 +1,40 @@
-let startInt = setInterval(simpleTimer, 1000);
 let seconds = 0;
 let minutes = 0;
-let startingTime = 2;
+
+let startingTime = 5;
+let timeString = '';
+
+let startInt = setInterval(simpleTimer, 1000);
 
 let timerSelector = document.querySelector('#timer')
 
 // 100 seconds - > 01 min 40 sec
 // 200 seconds - > 03 min 20 sec etc...
-function convertToMinuteSeconds(seconds){
+function convertToMinuteSeconds(seconds){  
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds - minutes * 60;
+    if (minutes.toString().length === 1){
+        let minutesArray = minutes.toString().split('');
+        minutesArray.unshift('0')
+        minutes = minutesArray.join('')
+    }
+    if (remainingSeconds.toString().length === 1){
+        let secondsArray = remainingSeconds.toString().split('');
+        secondsArray.unshift('0')
+        remainingSeconds = secondsArray.join('')
+        console.log(remainingSeconds)
+    }
+
+    timeString = `${minutes}:${remainingSeconds}`
     
+    return timeString;
 }
 
 function simpleTimer(){
     startingTime -= 1;
-    timerSelector.textContent = startingTime;
-    //if time reaches 0, calls stop function will will, well, stop timer
+    timerSelector.textContent = convertToMinuteSeconds(startingTime);
+    //    let convertedTime = convertToMinuteSeconds(startingTime);
+    //if time reaches 0, calls stop function will, well, stop timer
     if (startingTime == 0){
         stopFunction();
     }
